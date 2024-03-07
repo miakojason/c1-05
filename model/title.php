05a65edc681bda471ca570ee10e845ecc023ae9b
miakojason <miakojason@gmail.com>
2024-03-07 18:11:45 +0800

    新增功能正常 api add ok

---
+++ model/title.php
@@ -0,0 +1,17 @@
+<h1>新增標題區圖片</h1>
+<form action="./api/add.php" method="post" enctype="multipart/form-data">
+    <table>
+        <tr>
+            <td>標題區圖片:</td>
+            <td><input type="file" name="img" id=""></td>
+        </tr>
+        <tr>
+            <td>標題區替代文字:</td>
+            <td><input type="text" name="text" id=""></td>
+        </tr>
+    </table>
+    <div>
+        <input type="hidden" name="table" value="<?= $_GET['table']; ?>">
+        <input type="submit" value="新增"><input type="reset" value="重置">
+    </div>
+</form>
