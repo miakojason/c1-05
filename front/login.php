0b99f35bf85a84e4a2be76a599684ccf82ba9e71
miakojason <miakojason@gmail.com>
2024-03-07 18:20:14 +0800

    ok marquee

--- front/login.php
+++ front/login.php
@@ -8,7 +8,15 @@ if (isset($_GET['error'])) {
 ?>
 <div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
 	<marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
-	</marquee>
+	<?php
+		$rows = $Ad->all(['sh' => 1]);
+		foreach ($rows as $row) {
+		?>
+		<?=$row['text'];?>&nbsp;/&nbsp;
+		<?php
+		}
+		?>
+</marquee>
 	<div style="height:32px; display:block;"></div>
 	<!--正中央-->
 	<form method="post" action="./api/check.php">
