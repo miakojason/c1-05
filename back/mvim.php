46aae0b1e0d670da5f62c62b8cba91cb67e7ac0d
miakojason <miakojason@gmail.com>
2024-03-07 18:50:54 +0800

    back total+bottom->api edit_info ok， back mvim+image-ok

---
+++ back/mvim.php
@@ -0,0 +1,37 @@
+<div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
+    <p class="t cent botli">動畫圖片管理</p>
+    <form method="post" action="./api/edit.php">
+        <table width="100%">
+            <tr class="yel">
+                <td width="45%">動畫圖片</td>
+                <td width="7%">顯示</td>
+                <td width="7%">刪除</td>
+                <td></td>
+            </tr>
+            <?php
+            $rows = $DB->all();
+            foreach ($rows as $row) {
+            ?>
+                <tr>
+                    <td class="cent"><img src="./img/<?= $row['img']; ?>" style="width:100px;height:100px"></td>
+                    <td><input type="checkbox" name="sh[]" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? 'checked' : ''; ?>></td>
+                    <td>
+                        <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
+                        <input type="checkbox" name="del[]" value="<?= $row['id']; ?>">
+                    </td>
+                    <td><input type="button" value="更新圖片" onclick="op('#cover','#cvr','./model/upload.php?table=<?= $do; ?>&id=<?= $row['id']; ?>')"></td>
+
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+        <table style="margin-top:40px; width:70%;">
+            <tr>
+                <input type="hidden" name="table" value="<?= $do; ?>">
+                <td width="200px"><input type="button" onclick="op('#cover','#cvr','./model/<?= $do; ?>.php?table=<?= $do; ?>')" value="新增動畫圖片"></td>
+                <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
+            </tr>
+        </table>
+    </form>
+</div>
--- front/news.php
+++ front/news.php
@@ -1,26 +1,28 @@
 <div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
 	<marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
-	<?php
+		<?php
 		$rows = $Ad->all(['sh' => 1]);
 		foreach ($rows as $row) {
 		?>
-		<?=$row['text'];?>&nbsp;/&nbsp;
+			<?= $row['text']; ?>&nbsp;/&nbsp;
 		<?php
 		}
 		?>
-</marquee>
+	</marquee>
 	<div style="height:32px; display:block;"></div>
 	<!--正中央-->
 	<h3>更多最新消息區</h3>
 	<hr>
-	<ol>
+	<?php
+	$total = $News->count(['sh' => 1]);
+	$div = 5;
+	$pages = ceil($total / $div);
+	$now = $_GET['p'] ?? 1;
+	$start = ($now - 1) * $div;
+	$rows = $News->all(['sh' => 1], "limit $start,$div");
+	?>
+	<ol start="<?= $start + 1; ?>">
 		<?php
-		$total = $News->count(['sh' => 1]);
-		$div = 5;
-		$pages = ceil($total / $div);
-		$now = $_GET['p'] ?? 1;
-		$start = ($now - 1) * $div;
-		$rows = $News->all(['sh' => 1], "limit $start,$div");
 		foreach ($rows as $row) {
 		?>
 			<li class="sswww">
